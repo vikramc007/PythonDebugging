e8e4c11494de6db871938abbae499c1fc5e73b06
vikramc007 <vikrampschauhan@yahoo.com>
2025-01-21 02:25:23 -0800

    first commit

---
+++ inputFunctionInsidePrintFunction.py
@@ -0,0 +1,19 @@
+#print("Number of letters in your name: " + str(len(input("Enter your name: "))))
+#number_of_letters_in_name = "Number of letters in your name: "
+#length_of_name = len(input("Enter your name: "))
+#print(number_of_letters_in_name + str(length_of_name))
+#print(3 * 3 + 3 - 3 / 3 - 3)
+#print(3 * 3 + 3 / 3 - 3)
+#3 * 6 / (3 + 3) - 3
+#height = 1.65 
+#weight = 84
+
+# Write your code here.
+# Calculate the bmi using weight and height.
+#bmi = (weight/height) * (weight/height)
+
+#print(bmi)
+#print(1.65/86)
+#print(0.019186046511627905 * 0.019186046511627905)
+result = 6 + 4 / 2 - (1 * 2)
+print(f"The result is: {result}")
---
+++ vscodeProg1.py
@@ -0,0 +1,2 @@
+age = input("What is your age? ")
+print(f"Your age is: {age}")
